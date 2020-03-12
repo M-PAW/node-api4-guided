134e936f681449b5802028ff91b247a6e8ec0c92
Michael Walter <Michael_Patrick.Walter@outlook.com>
2020-03-12 10:33:57 -0600

    Prepping for Heroku Deploy

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const server = require('./api/server.js');
 
-const port = 4000;
+// Make the port be assigned by the server
+// heroku will plae the .PORT environment value to their server
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+
